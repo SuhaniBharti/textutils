bceb05b77e00266d5c9406f9637647f90fe83bd7
SuhaniBharti <156918664+SuhaniBharti@users.noreply.github.com>
2025-03-19 20:31:02 +0530

    Updates

---
+++ precache-manifest.e41e113c0f41cd86f2484d0b7461f35f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a86c0a12f444e02ff509943c3f15ef6c",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "6d3e85bcc3f68bb7e47d",
+    "url": "/textutils/static/css/main.76081950.chunk.css"
+  },
+  {
+    "revision": "0da3a3a7caac8e2de5c7",
+    "url": "/textutils/static/js/2.a5b0c296.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/textutils/static/js/2.a5b0c296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c22086003eed385c50",
+    "url": "/textutils/static/js/3.b5bd36bd.chunk.js"
+  },
+  {
+    "revision": "6d3e85bcc3f68bb7e47d",
+    "url": "/textutils/static/js/main.48fc18fc.chunk.js"
+  },
+  {
+    "revision": "79ea64f27fef7de3a5b2",
+    "url": "/textutils/static/js/runtime-main.d25e19fa.js"
+  }
+]);
